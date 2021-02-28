8c821026d80c72df73d8cbfe089c63ed5a83b5d6
petros <pit.trak@gmail.com>
2021-02-28 15:59:11 +0200

    implemented access token method getByID

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -4,6 +4,12 @@ import (
 	"bookstore_oauth-api/src/clients/cassandra"
 	"bookstore_oauth-api/src/domain/accesstoken"
 	"bookstore_oauth-api/src/utils/errors"
+
+	"github.com/gocql/gocql"
+)
+
+const (
+	queryGetAccessToken = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
 )
 
 // NewRepository will return a pointer to a dbRepository struct
@@ -22,10 +28,17 @@ type dbRepository struct {
 func (dr *dbRepository) GetByID(id string) (*accesstoken.AccessToken, *errors.RestErr) {
 	session, err := cassandra.GetSession()
 	if err != nil {
-		panic(err)
+		return nil, errors.NewInternalServerError(err.Error())
 	}
 	defer session.Close()
 
-	// TODO: impl get access token from CassandraDB
-	return nil, errors.NewInternalServerError("db conn not implemented yet")
+	var result accesstoken.AccessToken
+	if err := session.Query(queryGetAccessToken, id).Scan(&result.AccessToken, &result.UserID, &result.ClientID, &result.Expires); err != nil {
+		if err == gocql.ErrNotFound {
+			return nil, errors.NewNotFoundError("no access token found for given id")
+		}
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+
+	return &result, nil
 }
