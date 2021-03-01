f9f7ad635cef50c00f73abc5f7a22318dc8ed250
petros <pit.trak@gmail.com>
2021-03-01 16:52:28 +0200

    implemented create method

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	queryGetAccessToken = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
+	queryGetAccessToken    = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
+	queryCreateAccessToken = "INSERT INTO access_tokens(access_token, user_id, client_id, expires) VALUES (?, ?, ?, ?);"
 )
 
 // NewRepository will return a pointer to a dbRepository struct
@@ -20,6 +21,7 @@ func NewRepository() Repository {
 // Repository interface of DB
 type Repository interface {
 	GetByID(string) (*accesstoken.AccessToken, *errors.RestErr)
+	Create(accesstoken.AccessToken) *errors.RestErr
 }
 
 type dbRepository struct {
@@ -41,4 +43,22 @@ func (dr *dbRepository) GetByID(id string) (*accesstoken.AccessToken, *errors.Re
 	}
 
 	return &result, nil
+}
+
+func (dr *dbRepository) Create(at accesstoken.AccessToken) *errors.RestErr {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer session.Close()
+
+	if err := session.Query(queryGetAccessToken,
+		at.AccessToken,
+		at.UserID,
+		at.ClientID,
+		at.Expires).Exec(); err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+
+	return nil
 }
